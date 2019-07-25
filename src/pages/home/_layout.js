73a86297ca36e22110c58291fa3b3201c3295597
mario <whutch1991@sina.com>
2019-07-25 13:32:49 +0800

    更新

--- src/pages/home/_layout.js
+++ src/pages/home/_layout.js
@@ -31,11 +31,26 @@ function getValueByArray(menus,obj){
 class Home extends Component {
   state = {
     collapsed: false,
-    menus:[],
-    sMenu:[],
-    routeHistory: [],
-    routes:[]
+    routeHistory:[]
+    
   };
+  
+  routes=[]
+  menus = [{
+    key: 'sub-1', value: '系统菜单', icon: 'user', children: [{ key: '1', value: '系统设置', icon: 'user', route: '/home/sys' },
+    { key: '2', value: '用户管理', icon: 'user', route: '/home/user' },
+    { key: '3', value: '角色管理', icon: 'user', route: '/home/role' },
+    { key: '4', value: '资源管理', icon: 'user', route: '/home/resource' },
+    { key: '5', value: '组织架构', icon: 'user', route: '/home/org' },
+    { key: '6', value: '菜单管理', icon: 'user', route: '/home/menu' },
+    { key: '7', value: '字典管理', icon: 'user', route: '/home/dic' }]
+  },
+  {
+    key: 'sub-2', value: '宿舍管理', icon: 'user', children: 
+    [{ key: '8', value: '校区楼栋', icon: 'user', route: '/home/campus' },
+    { key: '9', value: '楼栋管理', icon: 'user', route: '/home/buildings' },
+    { key: '10', value: '房间管理', icon: 'user', route: '/home/room' }]
+  }]
   toggle = () => {
     this.setState({
       collapsed: !this.state.collapsed,
@@ -54,41 +69,18 @@ class Home extends Component {
 
   }
   componentDidMount() {
-    const menus = [{
-      key: 'sub-1', value: '系统菜单', icon: 'user', children: [{ key: '1', value: '系统设置', icon: 'user', route: '/home/sys' },
-      { key: '2', value: '用户管理', icon: 'user', route: '/home/user' },
-      { key: '3', value: '角色管理', icon: 'user', route: '/home/role' },
-      { key: '4', value: '资源管理', icon: 'user', route: '/home/resource' },
-      { key: '5', value: '组织架构', icon: 'user', route: '/home/org' },
-      { key: '6', value: '菜单管理', icon: 'user', route: '/home/menu' },
-      { key: '7', value: '字典管理', icon: 'user', route: '/home/dic' }]
-    },
-    {
-      key: 'sub-2', value: '宿舍管理', icon: 'user', children: 
-      [{ key: '8', value: '校区楼栋', icon: 'user', route: '/home/campus' },
-      { key: '9', value: '楼栋管理', icon: 'user', route: '/home/buildings' },
-      { key: '10', value: '房间管理', icon: 'user', route: '/home/room' }]
-    }]
-    const sMenu = menus.map(menu => {
-      return <SubMenu key={menu.key} title={<div><Icon type={menu.icon} /><span>{menu.value}</span></div>} >
-        {menu.children.map((child) => <Menu.Item key={child.key}><Link to={child.route}>{child.value}</Link></Menu.Item>)}
-      </SubMenu>
-    })
+    
     const location = this.props.location.pathname
-    const currentMenu =getValueByArray(menus,{route:location})
+    const currentMenu =getValueByArray(this.menus,{route:location})
     console.log('current',currentMenu)
-    // this.setState(preState=>({
-    //   sMenu:[...preState.sMenu,sMenu],
-    //   menus:[...preState.menus,menus],
-    //   routes:[...preState.routes,currentMenu]
-    // }))
-    this.setState({
-      sMenu:sMenu,
-      menus:menus,
-      routes:[currentMenu]
-    })
-    // this.menuChange(currentMenu)
-    // console.log('init',this.state)
+    if(currentMenu){
+      this.routes = [currentMenu]
+      this.setState({
+        routeHistory:this.routes.map((tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>))
+      })  
+      // this.state.routeHistory = this.routes.map((tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>))
+
+    }
   }
   // increment(state, props) {
   //   return {
@@ -96,50 +88,50 @@ class Home extends Component {
   //   }
   // }
   menuChange = (item) => {
-    let menus = this.state.menus
+    let menus = this.menus
     let tem = getValueByArray(menus,{key:item.key})
 
     
    
     // console.log('???',tem)
-    let index =this.state.routes.findIndex(r => r.key === tem.key)
+    let index =this.routes.findIndex(r => r.key === tem.key)
     if (index > -1) return
-    this.setState(preState=>({
-        routes:[...preState.routes,tem]
-    }))
-    console.log('newState',this.state)
-    // this.setState({
-    //   routeHistory: routes.map(tem => <div className={styles.cheader_item} key={'cheader' + tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this, tem)} type="close" /></div>)
-    // })
-
-     this.state.routeHistory = this.state.routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
+    this.routes = [...this.routes,tem]
+     this.state.routeHistory = this.routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
 
 
 
   }
 
   handelClose = (route) => {
-    let index = this.state.routes.findIndex(r => r.key === route.key)
-    console.log('index', index)
-    
-    this.setState(preState=>({
-      routes:preState.routes.filter(r=>r.key!==route.key)
-    }))
-    console.log('closestate',this.state)
-    console.log('closeprops', this.props)
-    // let backRoute = routes.length === 0 ? '/home' : routes[length - 1].route
+    // let index = this.routes.findIndex(r => r.key === route.key)
+    console.log('index', route)
+    this.routes = this.routes.filter(r=>r.key!==route.key)
+    console.log('routes',this.routes)
+    let length = this.routes.length
+    let backRoute = length === 0 ? '/home' : this.routes[length - 1].route
 
+    router.push(backRoute)
+    this.setState({
+      routeHistory:this.routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
+    })
+    // this.state.routeHistory = this.routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
     // this.props.history.goBack()
 
     // routeHistory = routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
 
 
-     this.state.routeHistory = this.state.routes.map(tem=><div className={styles.cheader_item} key={'cheader'+tem.key}><Link to={tem.route}>{tem.value}</Link><Icon onClick={this.handelClose.bind(this,tem)} type="close" /></div>)
 
 
   }
 
   render() {
+    
+    const sMenu =this.menus.map(menu => {
+      return <SubMenu key={menu.key} title={<div><Icon type={menu.icon} /><span>{menu.value}</span></div>} >
+        {menu.children.map((child) => <Menu.Item key={child.key}><Link to={child.route}>{child.value}</Link></Menu.Item>)}
+      </SubMenu>
+    })
 
     return (
       <Layout style={{ height: '100%' }}>
@@ -164,7 +156,7 @@ class Home extends Component {
 
             </div>
             <Menu theme="dark" style={{ borderRight: 'none' }} defaultSelectedKeys={['10']} mode="inline" onSelect={this.menuChange}>
-              {this.state.sMenu}
+              {sMenu}
             </Menu>
           </Sider>
           <Content>
